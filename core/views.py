386f0cc854269f69ad83a7e8c67f06c45d6cf3b5
AJRwin <arnwinrosales2@gmail.com>
2025-09-19 15:05:41 +0800

    assignment

---
+++ core/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def base(request):
+    return render(request, 'base.html', {'title': 'Base'})
+
+def home(request):
+    return render(request, 'home.html', {'title': 'Home'})
+
+def about(request):
+    return render(request, 'about.html', {'title': 'About'})
+
+def arnwin(request):
+    return render(request, 'arnwin.html', {'title': 'Arnwin'})
